8e89df9afb5a5da3328ed7caeec5472d7ad9a79a
Harry Tsang <harrytsang@fdssolutions.com.hk>
2026-03-27 17:56:35 +0800

    remove slug in utils

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,5 +1,3 @@
 pub mod validation; // 新增
-pub mod slug;
 
 pub use validation::*; // 新增
-pub use slug::*;
--- src/utils/slug.rs
+++
@@ -1,5 +0,0 @@
-use slug;
-
-pub fn slugify(text: &str) -> String {
-    slug::slugify(text)
-}
